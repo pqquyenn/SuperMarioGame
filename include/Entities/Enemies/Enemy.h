df8648cc26ceb4993a3aa39d1ddffbbb6de5e9f7
Luong Nhat Minh <luongnhatinh@gmail.com>
2026-07-24 20:37:17 +0700

    Build enemy and goomba

--- include/Entities/Enemies/Enemy.h
+++ include/Entities/Enemies/Enemy.h
@@ -4,12 +4,25 @@
 
 class Enemy : public Entity {
 protected:
-    float speed = 50.f;
-    int direction = -1; // -1 for left, 1 for right
+    float speed{50.f};
+    int direction{-1}; // -1: Left, 1: Right
+    bool isAlive{true};
+    bool squished{false};
 
 public:
-    Enemy(float x = 0.f, float y = 0.f) : Entity(x, y) {}
+    Enemy(float x = 0.f, float y = 0.f);
     virtual ~Enemy() = default;
 
     virtual void onStomped() = 0;
+    virtual void reverseDirection();
+
+    int getDirection() const;
+    void setDirection(int dir);
+
+    float getSpeed() const;
+    void setSpeed(float spd);
+
+    bool isSquished() const;
+    bool isEnemyAlive() const;
 };
+
