bdf0e5a9b29df6927dcc6d90374cdacf2cc6b674
PQQuyen <pqquyen040807@gmail.com>
2026-07-31 17:20:08 +0700

    add pausesate.h and handleinput() in pausestate.cpp

--- src/States/PauseState.cpp
+++ src/States/PauseState.cpp
@@ -5,7 +5,65 @@
 
 void PauseState::onEnter() {
     std::cout << "[PauseState] onEnter - Game da tam dung" << std::endl;
-    // TODO: Hien thi overlay "PAUSED", menu Resume/Quit
+
+    // --- Load font ---
+    const std::string fontPaths[] = {
+        "assets/fonts/press-start-2p.ttf",
+        "../assets/fonts/press-start-2p.ttf",
+        "../../assets/fonts/press-start-2p.ttf",
+        "../../../assets/fonts/press-start-2p.ttf"
+    };
+    fontLoaded = false;
+    for (const auto& path : fontPaths) {
+        if (font.loadFromFile(path)) {
+            fontLoaded = true;
+            break;
+        }
+    }
+
+    // --- Overlay den ban trong suot ---
+    overlay.setSize(sf::Vector2f(800.f, 600.f));
+    overlay.setFillColor(sf::Color(0, 0, 0, 140));  // Den ~55% opacity
+
+    // --- Text "PAUSED" ---
+    if (fontLoaded) {
+        pausedText.setFont(font);
+        pausedText.setString("PAUSED");
+        pausedText.setCharacterSize(36);
+        pausedText.setFillColor(sf::Color::White);
+        pausedText.setOutlineColor(sf::Color::Black);
+        pausedText.setOutlineThickness(2.f);
+        sf::FloatRect b = pausedText.getLocalBounds();
+        pausedText.setOrigin(b.left + b.width / 2.f, b.top + b.height / 2.f);
+        pausedText.setPosition(400.f, 200.f);
+
+        // --- "RESUME" ---
+        resumeText.setFont(font);
+        resumeText.setString("RESUME");
+        resumeText.setCharacterSize(16);
+        resumeText.setFillColor(sf::Color::White);
+        b = resumeText.getLocalBounds();
+        resumeText.setOrigin(b.left + b.width / 2.f, b.top + b.height / 2.f);
+        resumeText.setPosition(400.f, 320.f);
+
+        // --- "QUIT TO MENU" ---
+        quitText.setFont(font);
+        quitText.setString("QUIT TO MENU");
+        quitText.setCharacterSize(16);
+        quitText.setFillColor(sf::Color::White);
+        b = quitText.getLocalBounds();
+        quitText.setOrigin(b.left + b.width / 2.f, b.top + b.height / 2.f);
+        quitText.setPosition(400.f, 370.f);
+
+        // --- Selector ">" ---
+        selectorText.setFont(font);
+        selectorText.setString(">");
+        selectorText.setCharacterSize(16);
+        selectorText.setFillColor(sf::Color::White);
+    }
+
+    selectedIndex = 0;
+    updateSelectorPosition();
 }
 
 void PauseState::onExit() {
